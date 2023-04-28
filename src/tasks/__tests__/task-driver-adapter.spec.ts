c3d321d1b1f690a8b73893643ce4e34837628a39
Vitalino Silva <vitalinosilva@gmail.com>
2023-04-28 14:12:16 +0100

    added controller tests

--- src/tasks/__tests__/task-driver-adapter.spec.ts
+++ src/tasks/__tests__/task-driver-adapter.spec.ts
@@ -4,6 +4,7 @@ import request from "supertest";
 import {TasksResourcePath} from "../core/constants/tasks-resource.path.js";
 import type {TaskDTO} from "../core/dtos/task.dto.js";
 import {TasksController} from "../driver-adapters/tasks.controller.js";
+import {faker} from "@faker-js/faker";
 
 
 describe('Tasks driver adapter tests', () => {
@@ -29,7 +30,7 @@ describe('Tasks driver adapter tests', () => {
             expect(response.headers["Content-Type"]).contain(/json/);
             expect(response.status).toEqual(200);
 
-            expect(response.body).toEqual(expect.arrayContaining(<TaskDTO[]>[
+            expect(response.body).toStrictEqual(expect.arrayContaining(<TaskDTO[]>[
                 expect.objectContaining(<TaskDTO>{
                     id: expect.any(String),
                     title: expect.any(String),
@@ -48,10 +49,51 @@ describe('Tasks driver adapter tests', () => {
 
         it.todo('get /tasks route should return an empty collection TaskDTO if nothing exists on the data provider');
 
+        it('get /tasks/:id route should return a TaskDTO', async () => {
+
+            const allTasksResponse = await request(app)
+                .get(TasksResourcePath.RESOURCE)
+                .set('Accept', 'application/json');
+
+            const id = allTasksResponse.body[0].id;
+
+            const response = await request(app)
+                .get(`${TasksResourcePath.RESOURCE}/${id}`)
+                .set('Accept', 'application/json');
+
+            expect(response.headers["Content-Type"]).contain(/json/);
+            expect(response.status).toEqual(200);
+
+            expect(response.body).toStrictEqual(expect.objectContaining(<TaskDTO> {
+                id: expect.any(String),
+                title: expect.any(String),
+                description: expect.any(String),
+                priority: expect.any(String),
+                complete: expect.any(Boolean)
+            }));
+
+            expect(response.body.id).toMatch(uuidRegex);
+            expect(response.body.title.trim()).toBeTruthy();
+            expect(response.body.priority).toMatch(priorityOptionsRegex);
+
+        });
+
+
+        it('get /tasks/:id route should return null if nothing exists TaskDTO', async () => {
+
+            const response = await request(app)
+                .get(`${TasksResourcePath.RESOURCE}/${faker.datatype.uuid()}`)
+                .set('Accept', 'application/json');
+
+            expect(response.headers["Content-Type"]).contain(/json/);
+            expect(response.status).toEqual(404);
+            expect(response.body).toBeNull();
+
+        });
+
     });
 
-    it.todo('get /tasks/:id route should return a TaskDTO');
-    it.todo('get /tasks/:id route should return null if nothing exists TaskDTO');
+
     it.todo('post /tasks route should create/add a new Task to the data provider and return void after the process is complete');
     it.todo('post /tasks route should return an error if the request DTO is not correct');
     it.todo('put /tasks/:id route should update an existent Task on the data provider');
