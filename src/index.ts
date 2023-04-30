2ef7c788fcfe57dc5f8dcb7d57077dc0c8ce1d36
Vitalino Silva <vitalinosilva@gmail.com>
2023-04-30 22:24:42 +0100

    added security packages and updated app setup

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import cors from "cors";
 import Settings from "./settings/index.js";
 import {TasksController} from "./tasks/driver-adapters/tasks.controller.js";
+import helmet from "helmet";
 
 const port = Settings.port;
 const app = express();
 const router = express.Router();
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
+app.disable('x-powered-by');
 
 TasksController(app, router);
 
